6faed2354010fd0819903155d42805ac177a4cc7
Brian Flieck <flieckb@KOP01-BflieckMacBookPro2016.local>
2021-01-27 11:29:55 -0500

    intial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+  // <!-- 0e3a06de362672136f4d5f71a9cc90db api key BrianFlieckWeatherKe -->
+
+const key = "0e3a06de362672136f4d5f71a9cc90db";
+var city = "thorndale, PA";
+
+api.openweathermap.org/data/2.5/weather?q={city}&appid={key}
+
+};
