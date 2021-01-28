c7dde27e843aca3321c3d2552f58ce818d3e0a71
Brian Flieck <flieckb@KOP01-BflieckMacBookPro2016.local>
2021-01-28 18:10:19 -0500

    set up basic HTML, search box, button, and set up the first API call to open weather. Displaying some info in a div on the page

--- js/script.js
+++ js/script.js
@@ -2,9 +2,31 @@ $(document).ready(function () {
 
   // <!-- 0e3a06de362672136f4d5f71a9cc90db api key BrianFlieckWeatherKe -->
 
-const key = "0e3a06de362672136f4d5f71a9cc90db";
-var city = "thorndale, PA";
 
-api.openweathermap.org/data/2.5/weather?q={city}&appid={key}
 
-};
+$("#submit-btn").on("click", function () {
+
+  var searchTerm = $("#search-bar").val();
+
+
+fetch("http://api.openweathermap.org/data/2.5/weather?q="+searchTerm+"&appid=0e3a06de362672136f4d5f71a9cc90db")
+
+.then(Response => Response.json())
+.then(data => {
+ var nameValue = data['name'];
+ console.log(nameValue);
+ var tempvalue = data['main']['temp'];
+ console.log(tempvalue);
+ var descValue = data['weather'][0]['description'];
+ var iconValue = data['weather'][0]['icon'];
+$("#name").text(nameValue);
+$("#temp").text(tempvalue);
+$("#desc").text(descValue);
+$("#icon").text(iconValue);
+
+// //  .catch(err => alert("wrong city name"));
+})
+
+
+})
+});
