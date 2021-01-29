eec384588284ceb589a562bbbffa97f17035c191
Brian Flieck <flieckb@KOP01-BflieckMacBookPro2016.local>
2021-01-29 16:50:56 -0500

    changed HTML elements (index2.html) updated script to show more weather, includeing UV color colorizing)

--- js/script.js
+++ js/script.js
@@ -7,9 +7,10 @@ $(document).ready(function () {
 $("#submit-btn").on("click", function () {
 
   var searchTerm = $("#search-bar").val();
+ var icon = "http://openweathermap.org/img/wn/";
+ const apiKey = "0e3a06de362672136f4d5f71a9cc90db";
 
-
-fetch("http://api.openweathermap.org/data/2.5/weather?q="+searchTerm+"&appid=0e3a06de362672136f4d5f71a9cc90db")
+fetch("http://api.openweathermap.org/data/2.5/weather?q="+searchTerm+"&units=imperial&appid=0e3a06de362672136f4d5f71a9cc90db")
 
 .then(Response => Response.json())
 .then(data => {
@@ -18,15 +19,44 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q="+searchTerm+"&appid=0e3
  var tempvalue = data['main']['temp'];
  console.log(tempvalue);
  var descValue = data['weather'][0]['description'];
- var iconValue = data['weather'][0]['icon'];
-$("#name").text(nameValue);
-$("#temp").text(tempvalue);
-$("#desc").text(descValue);
-$("#icon").text(iconValue);
+ var iconValue = icon+data['weather'][0]['icon']+".png";
+ var windspeed = data['main']['feels_like'];
+ var longitude = data['coord']['lon'];
+ var latitude = data['coord']['lat'];
+$("#name").html("City: " + nameValue);
+$("#temp").html("Temperature: " + tempvalue + " &#8457");
+$("#desc").html("Weather Condition: "+ descValue);
+$("#icon").html("<img src=" + iconValue+ ">");
+$("#windspeed").html("Feels Like: " + windspeed + " &#8457");
+$("#latlon").html("Latitue: "+latitude + " Longitute: "+longitude);
 
-// //  .catch(err => alert("wrong city name"));
+// var uv = "http://api.openweathermap.org/data/2.5/uvi?lat="+latitude+"&lon="+longitude+"&appid="+apiKey;
+
+
+fetch("http://api.openweathermap.org/data/2.5/uvi?lat="+latitude+"&lon="+longitude+"&appid="+apiKey)
+.then(Response => Response.json())
+.then(data => {
+ var uvValue = data['value'];
+ console.log(uvValue);
+ $("#uv").html("UV index: " + uvValue);
+
+ if (uvValue < 2){
+  $("#uv").addClass("uvGreen");
+ }else if (uvValue >= 2 && uvValue <= 5){
+  $("#uv").addClass("uvYellow");
+ }else if(uvValue >=5 && uvValue <= 7){
+  $("#uv").addClass("uvOrange");
+ }else{
+  $("#uv").addClass("uvRed");
+ };
+
+//  $("#fiveday").empty();
+
+
+//  .catch(err => alert("wrong city name"));
+});
 })
 
 
-})
+});
 });
